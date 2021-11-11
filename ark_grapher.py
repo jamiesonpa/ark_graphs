99105f7248dd4d34fd3858fedd95f41b7863bda0
Pierce Jamieson <jamieson.pierce@gmail.com>
2021-11-11 09:17:16 -0600

    Update ark_grapher.py

--- ark_grapher.py
+++ ark_grapher.py
@@ -7,7 +7,6 @@ from pandas_datareader import DataReader
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
-import plotly.express as px
 
 def get_institutional_holders(ticks):
     finaldata = {}
