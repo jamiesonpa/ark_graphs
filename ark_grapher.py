fc81663bf5aaddcabe888f09a25125077ded4473
Pierce Jamieson <jamieson.pierce@gmail.com>
2021-11-11 09:02:52 -0600

    Update ark_grapher.py

--- ark_grapher.py
+++ ark_grapher.py
@@ -7,8 +7,6 @@ from pandas_datareader import DataReader
 import numpy as np
 import streamlit as st
 import matplotlib.pyplot as plt
-import bs4
-from bs4 import BeautifulSoup
 
 def get_institutional_holders(ticks):
     finaldata = {}
